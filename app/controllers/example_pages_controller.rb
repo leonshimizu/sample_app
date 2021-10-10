8bbbaa911c35641393d1c816905e1dd0af332e12
Leon <lmshimizu@gmail.com>
2021-10-10 08:07:08 -0700

    added world and made request.http

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,8 @@ class ExamplePagesController < ApplicationController
   def hello_action
     render json: {message: "hello"}
   end
+
+  def world_action
+    render json: {message: "world"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello_path", controller: "example_pages", action: "hello_action"
+  get "/world_path", controller: "example_pages", action: "world_action"
 end
